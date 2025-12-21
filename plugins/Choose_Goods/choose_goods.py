ac6664d213fb67d0676c839a5fdfc2e16a16590a
Sucrosato <sucrosato@gmail.com>
2025-12-21 15:55:58 +0800

    basic auto mine-and-sell process

---
+++ plugins/Choose_Goods/choose_goods.py
@@ -0,0 +1,70 @@
+from typing import Dict, Callable, Any
+from Util.Parser import JsonObject
+from Plugin.Interface import PluginInterface
+from loguru import logger
+import pyautogui
+import pytesseract
+
+class ChooseGoods(PluginInterface):
+    def __init__(self, manifest: Dict):
+        super().__init__(manifest)
+    
+    def register_functions(self) -> Dict[str, Callable]:
+        funcs: Dict[str, Callable] = {}
+
+        regions = [(200, 500, 100, 50),
+            (480, 500, 100, 50),
+            (760, 500, 100, 50),
+            (1040, 500, 100, 50),
+            (200, 838, 100, 50),
+            (480, 838, 100, 50),
+            (760, 838, 100, 50),
+            (1040, 838, 100, 50)]
+        
+        pos = [[200, 500],
+            [480, 500],
+            [760, 500],
+            [1040, 500],
+            [200, 838],
+            [480, 838],
+            [760, 838],
+            [1040, 838]]
+        
+        def get_prices(regions):
+            prices = []
+            # reader=easyocr.Reader(['en'], gpu=False, verbose=False)    # 少废话
+            custom_config = r'--oem 3 --psm 7 -c tessedit_char_whitelist=0123456789'
+            for i, region in enumerate(regions):
+                filename = str(i) + '.png'
+                screenshot = pyautogui.screenshot(region=region)
+                result = pytesseract.image_to_string(screenshot, config=custom_config)
+
+                # result = ocr.classification(screenshot)
+                # screenshot.save(filename)
+                # result = reader.readtext(filename, allowlist='0123456789', detail=0)
+                if result:
+                    prices.append(result.strip())
+                else:
+                    prices.append('-1')
+            return prices
+
+        def decide_target(prices):
+            target_price = '225'
+            for i, price in enumerate(prices):
+                if price == target_price:
+                    return i
+            return -1
+
+        def choose_goods(jsonObject: JsonObject):
+            target = decide_target(get_prices(regions))
+            # delay: int = jsonObject.content['delay']
+            # factor = self.meta.speed
+            if target != -1:
+                jsonObject.content['action'] = pos[target]
+            else:
+                jsonObject.content['event'] = "goto"
+                jsonObject.content['tolabel'] = "esc"
+        funcs['cg'] = choose_goods
+
+        return funcs
+    
---
+++ plugins/JudgeFunction/JudgeFunction.py
@@ -0,0 +1,22 @@
+from typing import Dict, Callable, Any
+from Util.Parser import JsonObject
+from Plugin.Interface import PluginInterface
+from loguru import logger
+import random
+
+class JudgeFunction(PluginInterface):
+    def __init__(self, manifest: Dict):
+        super().__init__(manifest)
+
+    def register_functions(self) -> Dict[str, Callable]:
+        funcs: Dict[str, Callable] = {}
+
+        def judge_function(jsonObject: JsonObject):
+            # delay: int = jsonObject.content['delay']
+            # factor = self.meta.speed
+            # jsonObject.content['delay'] = int(delay / factor)
+            jsonObject.content['action'] = [600, 480]
+
+        funcs['jf'] = judge_function
+
+        return funcs
